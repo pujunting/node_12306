884632ab5b267e5aa0cf93b975884a51b1cc127d
pjt <pu_jt@suixingpay.com>
2019-01-04 10:48:07 +0800

    席次监控由硬座硬卧改成硬卧软卧 邮件配置信息修改

--- main.js
+++ main.js
@@ -229,7 +229,7 @@ fs.readFile('config.json', 'utf-8', function (err, data) {
 	
 });
 
-var yz_temp = [], yw_temp = [];//保存余票状态
+var rw_temp = [], yw_temp = [];//保存余票状态
 /*
 * 查询余票
 */
@@ -324,9 +324,10 @@ function queryTickets(config) {
 		var data = '';
 		/*设置邮箱信息*/
 		var transporter = nodemailer.createTransport({
-			host: "smtp.163.com",//邮箱的服务器地址，如果你要换其他类型邮箱（如QQ）的话，你要去找他们对应的服务器，
+			host: "smtp.qq.com",//邮箱的服务器地址，如果你要换其他类型邮箱（如QQ）的话，你要去找他们对应的服务器，
 			secureConnection: true,
 			port: 465,//端口，这些都是163给定的，自己到网上查163邮箱的服务器信息
+			secure: true,
 			auth: {
 				user: config.your_mail,//邮箱账号
 				pass: config.mail_pass,//邮箱密码
@@ -369,8 +370,8 @@ function queryTickets(config) {
 					console.log('当天没有' + train_arr[j] + '这趟车次');
 					continue;
 				}
-				var yz = cur_train.yz_num;//硬座数目
 				var yw = cur_train.yw_num;//硬卧数目
+				var rw = cur_train.rw_num;//软卧数目
 				let trains = {
 					'高级软卧':cur_train.gr_num,
 					// 其他
@@ -418,32 +419,32 @@ function queryTickets(config) {
 				// // 动卧
 				// var srrb_num = cur_train.srrb_num;
 				var trainNum = cur_train.station_train_code;//车次
-				console.log('\n ' + trainNum + ' 车次的硬座余票数:', yz, ', 硬卧余票数:', yw, '。当前时间：' + getTime());
-				if (yz != '无' && yz != '--' || yw != '无' && yw != '--') {
-					if (yw_temp[j] == yw && yz_temp[j] == yz) {//当余票状态发生改变的时候就不发送邮件
+				console.log('\n ' + trainNum + ' 车次的硬卧余票数:', yw, ', 软卧余票数:', rw, '。当前时间：' + getTime());
+				if (yw != '无' && yw != '--' || rw != '无' && rw != '--') {
+					if (yw_temp[j] == yw && rw_temp[j] == rw) {//当余票状态发生改变的时候就不发送邮件
 						console.log(' ' + trainNum + '车次状态没改变，不重复发邮件');
 						continue;
 					}
 					var mailOptions = {
 						from: config.your_mail, // 发件邮箱地址
 						to: config.receive_mail || config.your_mail, // 收件邮箱地址，可以和发件邮箱一样
-						subject: trainNum + '有票啦，硬座：' + yz + '，硬卧：' + yw, // 邮件标题
+						subject: trainNum + '有票啦，硬卧：' + yw + '，软卧：' + rw, // 邮件标题
 						text: trainNum + '有票啦\n' + cur_train.from_station_name + '=============>' + cur_train.to_station_name + '\n出发日期：' + config.time + ',\n出发时间：' + cur_train.start_time + ',\n到达时间：' + cur_train.arrive_time + ',\n历时：' + cur_train.lishi, // 邮件内容
 					};
 
 					// 发邮件部分
-					(function (j, yz, yw) {
+					(function (j, yw, rw) {
 						transporter.sendMail(mailOptions, function (error, info) {
 							if (error) {
 								return console.log(error);
 							}
 							console.log(' 邮件已发送: ====================> ' + mailOptions.to);
 							yw_temp[j] = yw;//保存当前列车的余票数量
-							yz_temp[j] = yz;
+							rw_temp[j] = rw;
 						});
-					})(j, yz, yw);
+					})(j, yw, rw);
 				} else {
-					console.log(trainNum + '硬座/硬卧无票');
+					console.log(trainNum + '硬卧/软卧无票');
 				}
 			}
 			// fs.writeFile('./train.json',data);
